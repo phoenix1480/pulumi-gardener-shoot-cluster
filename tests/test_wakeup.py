2fb795c1d519d1f3db24131ff394795ba2ce1dc6
Gary <thomas.mackg@gmail.com>
2023-01-14 18:17:22 -0600

    Fix unit test

--- tests/test_wakeup.py
+++ tests/test_wakeup.py
@@ -6,7 +6,7 @@ class TestWakeUp(unittest.TestCase):
     def test_throws_an_error_when_msg_does_not_match(self):
         """Test WakeUp msg matches to scaffold pipeline"""
         msg = "Wake Up Pipeline"
-        assert "wake" in msg
+        assert "Wake" in msg
 
 if __name__ == '__main__':
     unittest.main()
